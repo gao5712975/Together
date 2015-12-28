d2241a511c23968b60e63b1dc232aafa30ac2f8b
gaoyuan <gaoyuan@sian.com>
2015-12-28 23:43:46 +0800

    local

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 /**
  * Created by Yuan on 2015/12/23.
  */
+'use strict';
+var express = require("express");
+var app = express();
+var path = require("path");
+var uuid = require('node-uuid');
+var port = 3000;
+
+/*加载中间件*/
+app.use(express.static('public'));
+//cookie
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+//body
+var bodyParser = require("body-parser");
+app.use(bodyParser());
+//morgan
+var morgan = require("morgan");
+morgan.token('id', function getId(req) {
+    return req.id
+});
+app.use(assignId);
+app.use(morgan("[:id :method :url :response-time]"));
+/*启动服务*/
+app.listen(port);
+
+function assignId(req, res, next) {
+    req.id = uuid.v4().replace(/\-/g, "");
+    next();
+}
--- app/schemas/user.js
+++ app/schemas/user.js
@@ -4,7 +4,10 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-    name:String,
+    name:{type:String},
     gender:{type:Number,min:0,max:1},
     age:{type:Number,min:0,max:150},
+    email:String,
+    create_time:{type:Date,default:Date.new},
+    update_time:{type:Date,default:Date.new}
 });
